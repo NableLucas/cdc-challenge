3635eb8775c8ae051be1810cbe4ceb9af1b51030
NableLucas <pnable.lucas@gmail.com>
2021-08-20 10:41:58 +0100

    initial commit

---
+++ src/main/java/com/nable/cdc/CdcChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.nable.cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcChallengeApplication.class, args);
+	}
+
+}
